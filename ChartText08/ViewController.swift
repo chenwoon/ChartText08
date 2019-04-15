09d506e9a03597d6fd766aa2e4376830b8555e0d
Thong Chen Woon <chenwoon_thong@hotmail.com>
2019-04-15 23:37:00 +0800

    Initial Commit

---
+++ ChartText08/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartText08
+//
+//  Created by Thong, Chen Woon on 15/4/19.
+//  Copyright © 2019 Thong, Chen Woon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
